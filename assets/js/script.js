b9e1de0c5fb4841745eff4d42c40c0f0d1107d79
secarrel <secarrel@outlook.com>
2023-08-21 17:02:05 +0100

    Modify: alert message for submit button

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function() {
     for (let button of buttons) {
         button.addEventListener("click", function() {
             if (this.getAttribute("data-type") === "submit") {
-                alert("You clicked Submit!");
+                alert("You clicked submit");
             } else {
                 let gameType = this.getAttribute("data-type");
                 alert(`You clicked ${gameType}`);
