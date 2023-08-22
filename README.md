a64d54ecdbb910f6376262362c55fc19ff99b0ae
secarrel <secarrel@outlook.com>
2023-08-22 12:29:19 +0100

    Add: division game type functionality

--- README.md
+++ README.md
@@ -10,8 +10,5 @@ A walkthrough project supplied by [Code Institute](https://codeinstitute.net/ful
 - HTML
 - CSS
 
-## Future Improvements
-The division game type is provided but doesn't work as a way to demonstrate 'else' actions but this game type is more challenging to code as it requires the user to answer with a float. This isn't very good UX as the answer could be to 9 decimal places for example. Making this game type work in a more user friendly way would be a good challenge for the future. 
-
 ## Credit
 This was a walkthrough project supplied by [Code Institute](https://codeinstitute.net/full-stack-software-development-diploma/?hsa_acc=8983321581&hsa_cam=1578649861&hsa_grp=1147890658588794&hsa_ad&hsa_src=o&hsa_tgt=kwd-71743597220113%3Aloc-188&hsa_kw=code%20institute&hsa_mt=e&hsa_net=adwords&hsa_ver=3&msclkid=96e9d238c1ea12152f48b6db168dd415&utm_source=bing&utm_medium=cpc&utm_campaign=CI%20-%20UK%20-%20Search%20-%20Brand&utm_term=code%20institute&utm_content=CI%20-%20UK%20-%20Search%20-%20Brand%20-%20Exact). I did not create or write any of this code independently. It is purely an aid to practice JavaScript. 
--- assets/js/script.js
+++ assets/js/script.js
@@ -37,6 +37,9 @@ function runGame(gameType) {
     // Creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+    let numD1 = Math.floor(Math.random() * 10) + 1;
+    let numD2 = Math.floor(Math.random() * 10) + 1;
+    let numD3 = numD1 * numD2
 
     if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
@@ -44,7 +47,9 @@ function runGame(gameType) {
         displaySubtractQuestion(num1, num2);
     } else if (gameType === "multiply") {
         displayMultiplyQuestion(num1, num2);
-    } else {
+    } else if (gameType === "division") {
+        displayDivisionQuestion(numD3, numD1);
+    }else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
     }
@@ -89,7 +94,9 @@ function calculateCorrectAnswer() {
         return [operand1 - operand2, "subtract"];
     } else if (operator === "x") {
         return [operand1 * operand2, "multiply"];
-    } else {
+    } else if (operator === "/") {
+        return [operand1 / operand2, "division"];
+    }else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
     }
@@ -138,5 +145,13 @@ function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "x";
+
+}
+
+function displayDivisionQuestion(operand1, operand2) {
+
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "/";
 
 }
